882a4dfefccb8d3c1b4d011aa858e0b47ed25f0e
Roberto <robe_edu@hotmail.com>
2016-11-14 17:55:18 +0100

    auntenticacion

--- Autenticacion/Autenticacion/Program.cs
+++ Autenticacion/Autenticacion/Program.cs
@@ -10,6 +10,53 @@ namespace Autenticacion
     {
         static void Main(string[] args)
         {
+            int Menu = 5;
+            while (Menu != 0) {
+                Console.Clear();
+                Console.WriteLine("1- Alta Usuario");
+                Console.WriteLine("2- Autenticarse");
+                Console.WriteLine("0- Salir");
+                try { 
+                Menu = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Valor incorrecto,presiona una tecla para continuar");
+                    Console.ReadKey();
+                }
+                
+                if (Menu == 1)
+                {
+                    String Password = null;
+                    String Nombre = null;
+                    Console.WriteLine("Introduce el nombre de usuario");
+                    Nombre = Console.ReadLine();
+                    Console.WriteLine("Introduce el password");
+                    ConsoleKeyInfo key;
+                    
+                    do
+                    {
+                        key = Console.ReadKey(true);
+                        if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                        {
+                            Password += key.KeyChar;
+                            Console.Write("*");
+                        }
+                        else if(key.Key==ConsoleKey.Backspace)
+                            {
+                            if ((Password.Length>0))
+                            {
+                                Console.Write("\b\b");
+                            }
+                        }
+                    } while (key.Key != ConsoleKey.Enter);
+                    Console.WriteLine();
+                    Console.WriteLine("Usuario Creado Correctamente");
+                    Console.ReadKey();
+                }
+
+
+            }
         }
     }
 }
