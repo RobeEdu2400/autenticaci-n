5804b3dfb6d8a4302ff2d2b97e49a3870c386c5a
Roberto <robe_edu@hotmail.com>
2016-11-24 18:57:56 +0100

    update

--- Autenticacion/Autenticacion/Program.cs
+++ Autenticacion/Autenticacion/Program.cs
@@ -6,31 +6,24 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Autenticacion
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Autenticacion{
+    class Program{
+        static void Main(string[] args){
             int Menu=0;
-            do
-            {
+            do{
                 Console.Clear();
                 Console.WriteLine("1- Alta Usuario");
                 Console.WriteLine("2- Autenticarse");
                 Console.WriteLine("0- Salir");
-                try
-                {
+                try{
                     Menu = Convert.ToInt32(Console.ReadLine());
                 }
-                catch
-                {
+                catch{
                     Console.WriteLine("Valor incorrecto,presiona una tecla para continuar");
                     Console.ReadKey();
                 }
 
-                if (Menu == 1)
-                {
+                if (Menu == 1){
                     String Password = null;
                     String Nombre = null;
                     Console.WriteLine("Introduce el nombre de usuario");
@@ -38,18 +31,14 @@ namespace Autenticacion
                     Console.WriteLine("Introduce el password");
                     ConsoleKeyInfo key;
 
-                    do
-                    {
+                    do{
                         key = Console.ReadKey(true);
-                        if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
-                        {
+                        if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter){
                             Password += key.KeyChar;
                             Console.Write("*");
                         }
-                        else if (key.Key == ConsoleKey.Backspace)
-                        {
-                            if ((Password.Length > 0))
-                            {
+                        else if (key.Key == ConsoleKey.Backspace){
+                            if ((Password.Length > 0)){
                                 Console.Write("\b\b");
                             }
                         }
@@ -57,23 +46,22 @@ namespace Autenticacion
                     Console.WriteLine();
                     Console.WriteLine("Usuario Creado Correctamente");
                     Console.ReadKey();
-
+                    
                     byte[] salt;
                     new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
 
-                    String resultadoSalt = BitConverter.ToString(salt, 0);
+                    string resultadoSalt;
+                    string resultadoHash;
 
                     var prueba = new Rfc2898DeriveBytes(Password, salt, 1);
                     byte[] hash = prueba.GetBytes(32);
 
-                    String resultadoHash = BitConverter.ToString(hash, 0);
+                    resultadoSalt = Convert.ToBase64String(salt);
+                    resultadoHash = Convert.ToBase64String(hash);
 
-                    Console.WriteLine("Introduce el nombre del archivo:");
-                    String nombre = null;
-                    while (nombre == null)
-                        nombre = Console.ReadLine();
-
-                    File.AppendAllText(nombre,Nombre+" "+resultadoSalt+" "+resultadoHash+string.Format(Environment.NewLine));
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"D:\C#\autenticaci-n\Autenticacion\Autenticacion\bin\Debug\WriteLines.txt", true)){
+                        file.WriteLine(Nombre+('-')+ resultadoSalt + ('-') + resultadoHash);
+                    }                        
                 }
             } while (Menu != 0);
         }
